41d277c5158e9456d01b50959be846f1ee259056
eholguin1 <eholguin@ucsd.edu>
2024-10-09 19:30:13 -0700

    Update App.tsx

--- Lab2/my-app/src/App.tsx
+++ Lab2/my-app/src/App.tsx
@@ -1,66 +1,108 @@
 import './App.css';
 import { Label, Note } from "./types";
-import { dummyNotesList } from "./constants";
-import ToggleTheme, { AddToFav } from './hooksExercise';
+import  AddToFav  from './hooksExercise';
 import { useState } from 'react';
 import { ThemeContext, themes } from './themeContext'; 
 
 function App() {
  const [currentTheme, setCurrentTheme] = useState(themes.light);
  const [favList, setFavList] = useState<string[]>([]);
+
+ const [notes, setNotes] = useState<Note[]>([]); 
+
+ const initialNote = {
+   id: -1,
+   title: "",
+   content: "",
+   label: Label.other,
+ };
+ const [createNote, setCreateNote] = useState(initialNote);
  
  return (
-  <ThemeContext.Provider value={currentTheme}>
-   <div className='app-container'
-   style={{
-    background: currentTheme.background,
-    color: currentTheme.foreground,
-    padding: "20px",
-  }}>
-    <form className="note-form">
-      <button onClick={() => {
-        setCurrentTheme(currentTheme === themes.dark ? themes.light : themes.dark);
-      }}>
-        Toggle theme
-      </button>
-       <div><input placeholder="Note Title"></input></div>
+  <ThemeContext.Provider value={currentTheme}>
+   <div className='app-container'
+   style={{
+    background: currentTheme.background,
+    color: currentTheme.foreground,
+    padding: "20px",
+  }}>
+    <form className="note-form">
+      <button onClick={(e) => {
+        e.preventDefault();
+        setCurrentTheme(currentTheme === themes.dark ? themes.light : themes.dark);
+      }}>
+        Toggle theme
+      </button>
+       <div>
+        <input
+            placeholder="Note Title"
+            onChange={(event) =>
+              setCreateNote({ ...createNote, title: event.target.value })}
+            required>
+        </input>
+       </div>
 
-       <div><textarea placeholder='Note Content'></textarea></div>
+       <div>
+        <textarea placeholder='Note Content'
+          onChange={(event) =>
+            setCreateNote({ ...createNote, content: event.target.value })}
+          required>
+        </textarea>
+       </div>
 
-       <div>
-        <select className='selectionField'>
-            <option value="">Choose One</option>
-            <option value="personal">Personal</option>
-            <option value="work">Work</option>
-            <option value="study">Study</option>
-            <option value="other">Other</option>
-        </select>
-       </div>
+       <div>
+        <select className='selectionField' 
+          onChange={(event) =>
+              setCreateNote({ ...createNote, label: event.target.value as Label, id: notes.length+1})}
+            required
+        >
+            <option value="" disabled selected>Choose One</option>
+            <option value={Label.personal}>Personal</option>
+            <option value={Label.work}>Work</option>
+            <option value={Label.study}>Study</option>
+            <option value={Label.other}>Other</option>
+        </select>
+       </div>
 
-       <div><button className='submitButton' type="submit">Create Note</button></div>
-       <h2>List of favorites:</h2>
-       {favList.map((item) => (
-         <>{item} <br/></>
-       ))}
-    </form>
-    
-    <div className="notes-grid" >
-       {dummyNotesList.map((note) => (
-         <div
-           key={note.id}
-           className="note-item">
-           <div className="notes-header">
-            <AddToFav title = {note.title} favList={favList} setFavList={setFavList}/>
-             <button>x</button>
-           </div>
-           <h2> {note.title} </h2>
-           <p> {note.content} </p>
-           <p> {note.label} </p>
-         </div>
-       ))}
-     </div>
-   </div>
-  </ThemeContext.Provider>
+       <div>
+        <button className='submitButton' type="submit"
+          onClick={(e) => {
+            setNotes((prevNotes) => [...prevNotes, createNote]); 
+            e.preventDefault();
+          }}
+        >
+          Create Note
+        </button>
+       </div>
+       <h2>List of favorites:</h2>
+       {favList.map((item) => (
+         <>{item} <br/></>
+       ))}
+    </form>
+    
+    <div className="notes-grid" >
+       {notes.map((note) => (
+         <div
+           key={note.id}
+           className="note-item">
+           <div className="notes-header">
+            <AddToFav title = {note.title} favList={favList} setFavList={setFavList}/>
+             <button
+              onClick={(e) => {
+                e.preventDefault();
+                setNotes(notes.filter(item => item !== note))
+              }}>
+              x
+             </button>
+           </div>
+           <h2> {note.title} </h2>
+           <p> {note.content} </p>
+           <p> {note.label} </p>
+         </div>
+       ))}
+     </div>
+   </div>
+  </ThemeContext.Provider>
  );
 }
 
